3cb0f5aed1b0d17a7538c1f981ea3817085211da
gametag7892 <78163768+gametag7892@users.noreply.github.com>
2025-09-05 21:04:33 -0300

    Exercicios 1 & 2 & 3
    Todos os exercicios solucionados.

---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+public class Carro
+{
+    private string modelo;
+    private int velocidadeAtual;
+
+    public string Modelo
+    {
+        get { return modelo; }
+    }
+
+    public int VelocidadeAtual
+    {
+        private set
+        {
+            if (value <= 0)
+            {
+               velocidadeAtual = 0;
+            }
+            else
+            {
+                velocidadeAtual = value;
+            }
+        }
+
+        get { return velocidadeAtual; }
+    }
+
+    public Carro(string model)
+    {
+        modelo = model;
+    }
+
+    public void Acelerar(int valor)
+    {
+        VelocidadeAtual += valor;
+    }
+
+    public void Frear(int valor)
+    {
+        VelocidadeAtual -= valor;
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Carro c = new Carro("Corolla");
+        c.Acelerar(50);
+        Console.WriteLine(c.VelocidadeAtual);
+        c.Frear(30);
+        Console.WriteLine(c.VelocidadeAtual);
+        c.Frear(50);
+        Console.WriteLine(c.VelocidadeAtual);
+    }
+}
