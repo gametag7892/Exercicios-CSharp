27181336173605020768cf3ec4cbf907e9d6dd1b
gametag7892 <78163768+gametag7892@users.noreply.github.com>
2025-09-05 21:12:28 -0300

    Exercicio da aula 1 de programação

---
+++ AulaProgramacao/AulaProgramacao/aula2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+public class Produto
+{
+	private string _nome;
+	private decimal _preco;
+
+	public string Nome {
+		get { return _nome; } 
+	}
+
+	public decimal Preco {
+		get { return _preco; }
+
+		set
+		{
+			if (_preco > 0)
+			{
+				_preco = value;
+			}
+		}
+	}
+
+	public Produto(string name, decimal price) 
+	{
+		_nome = name;
+		_preco = price;
+
+	}
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Preço: {Preco}");
+
+    }
+
+}
+
+public class aula2
+{
+	public static void Main()
+	{
+		Produto p = new Produto("Celular", 1500);
+		p.ExibirDetalhes();
+		p.Preco = -200;
+	}
+}
