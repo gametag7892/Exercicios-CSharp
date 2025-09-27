a9a0f51403e3a747875e46943cca83036b3208e4
gametag7892 <eduardosoaresmota3@gmail.com>
2025-09-26 21:52:47 -0300

    Exericicios dos Roteiros 3 e 4

---
+++ Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Item
+{
+    public string nomeItem;
+}
+public class Pedido
+{
+    public Item item;
+
+    public Pedido()
+    {
+        item = new Item();
+    }
+
+    public void pedirItem()
+    {
+        item.nomeItem = "Prato";
+        Console.WriteLine("Quero um " + item.nomeItem);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Pedido pedido = new Pedido();
+        pedido.pedirItem();
+    }
+}
